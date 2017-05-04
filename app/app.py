7f7addc3595fa950b44e4999883333c85e223695
JamesMBartlett <jmabartlett@berkeley.edu>
2017-05-04 00:22:36 +0000

    new non zappa thing

---
+++ app/app.py
@@ -0,0 +1,129 @@
+from __future__ import print_function
+import requests
+import os
+# --------------- Helpers that build all of the responses ----------------------
+
+def build_speechlet_response(output, should_end_session):
+    return { 'version': '1.0',
+        'response': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': output
+            },
+            'reprompt': {
+                'outputSpeech': {
+                    'type': 'PlainText',
+                    'text': "",
+                }
+            },
+            'shouldEndSession': should_end_session
+        }
+    }
+
+
+# --------------- Functions that control the skill's behavior ------------------
+
+def get_welcome_response():
+    """ If we wanted to initialize the session to have some attributes we could
+    add those here
+    """
+
+    session_attributes = {}
+    card_title = "Welcome"
+    speech_output = "hi! This is an alexa prize social bot, I can talk about many things, what would you like to talk about?"
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    should_end_session = False
+    return build_speechlet_response( speech_output, should_end_session)
+
+
+def handle_session_end_request():
+    card_title = "Session Ended"
+    speech_output = ""
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
+    return build_speechlet_response( speech_output, should_end_session)
+
+def next_round(intent, session):
+    print(intent)
+    try:
+        response = requests.post(os.environ['DISCRIMINATOR_URI'], data = {'text': intent['slots']['All']['value'], 'sessionId': session['sessionId'], 'user': session['user']['userId']})
+        return build_speechlet_response( response.text, False)
+    except (KeyError, requests.exceptions.RequestException):
+        return build_speechlet_response( "My servers aren't available at this time.", False)
+# --------------- Events ------------------
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
+
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return get_welcome_response()
+
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "AllIntent":
+        return next_round(intent, session)
+    elif intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        raise ValueError("Invalid intent")
+
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # add cleanup logic here
+    return handle_session_end_request()
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    if (event['session']['application']['applicationId'] !=
+             "amzn1.ask.skill.52e109d3-c413-4d88-821d-25414a12fa9a"):
+         raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
